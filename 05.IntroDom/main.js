5fb98921c976567676e03c7228eb20b438a213c3
tveromc <you@example.com>
2024-08-28 19:51:40 -0600

    ✨ agregue comentarios

--- 05.IntroDom/main.js
+++ 05.IntroDom/main.js
@@ -9,27 +9,54 @@
 
   // voy a definir una lista 
 
-  const opciones = ['piedra', 'papel', 'tijera']
-  console.log(opciones)
+  const opciones = ['piedra', 'papel' , 'tijera']
+  // console.log(opciones)
+ // función para jugar el juego 
+ function jugar(opcionSeleccionada) {
+  // hacer que la computadora elija una opción de manera aleatoria
+  const opcionComputadora =  opciones [Math.floor(Math.random() * 3)]
 
-  // función para jugar el juego
-  function jugar(opcionSeleccionada) {
-    // hacer que la computadora elija una opción de manera aleatorio
- const opcionComputadora = opciones [Math.floor(Math.random() * 3)]
-
- // determinar al ganador 
+  // determinar al ganador
   let resultado = ''
-  if(opcionSeleccionada === opcionComputadora){
-     resultado = 'Empate'
+  if(opcionSeleccionada === opcionComputadora) {
+      resultado = 'Empate'
   } else if (
-    (opcionSeleccionada === 'piedra' && opcionComputadora === 'tijera') ||
-    (opcionSeleccionada === 'papel' && opcionComputadora === 'piedra') ||
-    (opcionSeleccionada === 'tijera' && opcionComputadora === 'papel')
-  ){
-    resultado = 'Ganaste'
-  }else{
-    resultado = 'Perdiste'
+      (opcionSeleccionada === 'piedra' && opcionComputadora === 'tijera') ||
+      (opcionSeleccionada === 'papel' && opcionComputadora === 'piedra') ||
+      (opcionSeleccionada === 'tijera' && opcionComputadora === 'papel')
+  ) {
+      resultado = 'Ganaste'
+  }else {
+      resultado = 'Perdiste'
   }
- // actualizar le resultado del mensaje
-  resultadoMensaje.textContent = `Elegiste ${opcionSeleccionada}, la máquina eligio ${opcionComputadora}, ${resultado}`
+  // actulizar el resultado del mensaje 
+  resultadoMensaje.textContent = `Elegiste ${opcionSeleccionada}, la máquina eligio ${opcionComputadora} . ${resultado} `
+}
+
+  // Agregar un evento AddEventListener 
+  // recorrer el arreglo o la lista llamada opciones
+  for(let i = 0; i <= opcionJugador.length; i++){
+    const opcion = opcionJugador[i]
+    const valor = opcion // capturar el valor de opción 
+    opcion.addEventListener('click',
+      // funcion anonima que ejecuta a la funcion:  
+      // ? function jugar(opcionSeleccionada)
+      function (){
+        const opcionSeleccionada = valor.dataset.opcion
+        jugar(opcionSeleccionada)
+      }
+    )
   }
+
+// otra forma de hacerlo
+// agregar un AddEventListener para cada oción del buton 
+ // recorrer el arreglo o lista llamada opciones
+//  opcionJugador.forEach(opcion => {
+//   opcion.addEventListener('click', () => {
+//       const opcionSeleccionada = opcion.dataset.opcion 
+//       jugar(opcionSeleccionada)
+//   })
+// })
+
+
+  
